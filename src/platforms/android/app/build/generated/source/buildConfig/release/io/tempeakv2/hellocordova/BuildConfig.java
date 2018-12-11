c16c7a28df4f98e46dafdeb3ba518563a9e0e49c
arniel <arnielmontero@gmail.com>
2018-12-11 10:34:54 +0800

    update version

--- src/platforms/android/app/build/generated/source/buildConfig/release/io/tempeakv2/hellocordova/BuildConfig.java
+++ src/platforms/android/app/build/generated/source/buildConfig/release/io/tempeakv2/hellocordova/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "io.tempeakv2.hellocordova";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 13100;
+  public static final int VERSION_CODE = 13400;
   public static final String VERSION_NAME = "1.0.0";
 }
