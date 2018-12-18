132bc686deac5ecf8c0b36e23a78da131a1cfc39
arniel <arnielmontero@gmail.com>
2018-12-18 17:41:42 +0800

    Update some funcitons
    Update function

--- src/platforms/android/app/src/main/assets/www/js/registration.js
+++ src/platforms/android/app/src/main/assets/www/js/registration.js
@@ -1,101 +1,109 @@
 $(document).ready(function (){
 
-  $("#register").click(function(e) {
 
-	var role = $('#role').val(),
-	password = $('#password').val(),
-	cpassword = $('#c_password').val();
-	
-	 var check1 = $('#customCheck1').is(":checked");
 
-	 if(check1 === false){
-		 
-	 }else{
-	 
-	 if (cpassword !== password ) { 
-				
-	 } else { 
+  $("#regtarget").submit(function(e) {
 
-		
-	 
-		var role = $("#role").val(),
-			firstname = $("#firstname").val(),
-			lastname = $("#lastname").val(),
-			email = $("#email").val(),			
-			dateofbirth = $("#date").val();
-	
-		switch(role){
-			case '0':
-				
-				var dataString = {
-					"role":role,
-					"firstname":firstname,
-					"lastname":lastname,
-					"email":email,
-					"password":password,
-					"dateofbirth":dateofbirth,			
-				};
-				
-				var dorole = 'Businessregistration';
-				
-			break;
-			
-			case '1':
-				
-				
-				var dorole = 'Registration2';
-				
-				var clinicalstaff = $("#clinicalstaff").val();					
-				if($("#speciality").val() != null){
-					var dataString = {
-						"role":role,
-						"firstname":firstname,
-						"lastname":lastname,
-						"email":email,
-						"password":password,
-						"dateofbirth":dateofbirth,			
-						"clinical_staff":clinicalstaff,	
-						"speciality":$("#speciality").val(),				
-					};			
-				}else{					
-					var dataString = {
-					"role":role,
-					"firstname":firstname,
-					"lastname":lastname,
-					"email":email,
-					"password":password,
-					"dateofbirth":dateofbirth,			
-					"clinical_staff":clinicalstaff,						
-					};
-				}
-			break;			
-		}
-		$.ajax({
-			type: "POST",
-			url: hosturl +"register",
-			data: dataString,
-			crossDomain: true,
-			cache: false,
-			success: function(data) {				
-				if(data == 'false'){
-					alert('Email is already use.');
-				}else{									
-					sessionStorage.setItem("key", data);					
-				
-					setTimeout(function(){											
-						window.location.href="./"+dorole+".html?key="+data;
-					}, 2000);
+    var password = document.getElementById("password")
+      , c_password = document.getElementById("c_password");
 
-				}				
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				alert(textStatus);
-			}
-		});
-		e.preventDefault();
-		e.stopPropagation();
-	 }
-	}
+      function validatePassword(){
+        if(password.value != c_password.value) {
+          c_password.setCustomValidity("Passwords Don't Match");
+        } else {
+          c_password.setCustomValidity('');
+        }
+      }
+
+      password.onchange = validatePassword;
+      c_password.onkeyup = validatePassword;
+
+
+      if(password.value == c_password.value) {
+        var role = $("#role").val(),
+        			firstname = $("#firstname").val(),
+        			lastname = $("#lastname").val(),
+        			email = $("#email").val(),
+        			passwords = $('#password').val(),
+        			dateofbirth = $("#date").val();
+
+        		switch(role){
+        			case '0':
+
+        				var dataString = {
+        					"role":role,
+        					"firstname":firstname,
+        					"lastname":lastname,
+        					"email":email,
+        					"password":passwords,
+        					"dateofbirth":dateofbirth,
+        				};
+
+        				var dorole = 'Businessregistration';
+
+        			break;
+
+        			case '1':
+
+
+        				var dorole = 'Registration2';
+
+        				var clinicalstaff = $("#clinicalstaff").val();
+        				if($("#speciality").val() != null){
+        					var dataString = {
+        						"role":role,
+        						"firstname":firstname,
+        						"lastname":lastname,
+        						"email":email,
+        						"password":passwords,
+        						"dateofbirth":dateofbirth,
+        						"clinical_staff":clinicalstaff,
+        						"speciality":$("#speciality").val(),
+        					};
+        				}else{
+        					var dataString = {
+        					"role":role,
+        					"firstname":firstname,
+        					"lastname":lastname,
+        					"email":email,
+        					"password":passwords,
+        					"dateofbirth":dateofbirth,
+        					"clinical_staff":clinicalstaff,
+        					};
+        				}
+        			break;
+        		}
+        		$.ajax({
+        			type: "POST",
+        			url: hosturl +"register",
+        			data: dataString,
+        			crossDomain: true,
+        			cache: false,
+        			success: function(data) {
+        				if(data == 'false'){
+        					alert('Email is already use.');
+        				}else{
+        					sessionStorage.setItem("key", data);
+
+        					setTimeout(function(){
+        						window.location.href="./"+dorole+".html?key="+data;
+        					}, 2000);
+
+        				}
+        			},
+        			error: function(jqXHR, textStatus, errorThrown) {
+        				alert(textStatus);
+        			}
+        		});
+
+        }else{
+             c_password.setCustomValidity("Passwords Don't Match");
+        }
+
+
+       e.preventDefault();
+        e.stopPropagation();
+
 	
   });
 });
